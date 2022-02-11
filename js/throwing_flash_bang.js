f7401e92615ea1f44ba40608450056e312dc250a
Brian <nairb725@gmail.com>
2022-02-11 09:17:39 +0100

    fix flashed done

--- js/throwing_flash_bang.js
+++ js/throwing_flash_bang.js
@@ -1,16 +1,20 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-canvas.style.display = "none";
-canvas.style.position = 'absolute';
-ctx.fillStyle = 'green';
-ctx.fillRect(0, 0, 5000, 5000);
-
 function white_screen(){
+  var white_screen = document.getElementById("container_csgo");
     var throwing_flashbang = new Audio("../img/throwing_flashbang.mp3");
     var bouncing_explose = new Audio("../img/bouncing_flash_sound.mp3");
     throwing_flashbang.play();
     function delay(time) {
         return new Promise(resolve => setTimeout(resolve, time));
       }
-      delay(1000).then(() => canvas.style.display = "block",bouncing_explose.play()).then(()=>canvas.style.display = "none");
-}
+      var get_blinded = document.getElementById("blinded_gif")
+      var background = document.getElementById("section_csgo")
+      var body = document.getElementById("body")
+      delay(1000).then(() => bouncing_explose.play());
+      delay(2000).then(() => white_screen.style.display = "none");
+      delay(2000).then(() => get_blinded.style.display="block");
+      delay(2000).then(() => background.style.background="linear-gradient(white, rgb(255,255,255), white)");
+      delay(2000).then(() => body.style.background="white");
+      delay(3800).then(() => get_blinded.style.display="none");
+      delay(3800).then(() => background.style.background="linear-gradient(white, rgb(161, 161, 161), black)");
+      delay(3800).then(() => white_screen.style.display = "block");
+    }
