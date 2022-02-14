94ef1941985159b7d58be3cf89ff0086db3d592c
Brian <nairb725@gmail.com>
2022-02-14 16:10:26 +0100

    no img flashed

--- js/throwing_flash_bang.js
+++ js/throwing_flash_bang.js
@@ -1,5 +1,4 @@
 function white_screen(){
-  var white_screen = document.getElementById("container_csgo");
     var throwing_flashbang = new Audio("../img/throwing_flashbang.mp3");
     var bouncing_explose = new Audio("../img/bouncing_flash_sound.mp3");
     throwing_flashbang.play();
@@ -9,6 +8,8 @@ function white_screen(){
       var get_blinded = document.getElementById("blinded_gif")
       var body = document.getElementById("body")
       delay(1000).then(() => bouncing_explose.play());
-      delay(1800).then(() => body.style.display = "none");
+      delay(1900).then(() => body.style.display = "none");
+      delay(1900).then(() => get_blinded.style.display = "block");
+      delay(3800).then(() => get_blinded.style.display = "none");
       delay(3800).then(() => body.style.display = "block");
     }
